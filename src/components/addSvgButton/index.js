13e33d678b2b77d7d64cdfb8960ede18a9f68ce1
Haoming Wu <slodin_741@hotmail.com>
2018-11-06 22:01:38 -0800

    linked libaries for IOS, added icons on navigator bar with function

--- src/components/addSvgButton/index.js
+++ src/components/addSvgButton/index.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import {Button, StyleSheet} from 'react-native';
+import {Button, StyleSheet,TouchableHighlight, Alert} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default class AddSvgButton extends Component{
   constructor(props) {
@@ -7,7 +9,22 @@ export default class AddSvgButton extends Component{
    }
   render() {
     return (
-      <Button title="ee"/>
+      <TouchableHighlight onPress={this.addReadTextArea}>
+        <Icon name="plus" size={30} color="#111111" />
+      </TouchableHighlight>
+    )
+  }
+
+  addReadTextArea(){
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      { cancelable: false }
     )
   }
 }
